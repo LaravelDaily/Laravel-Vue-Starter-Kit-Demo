69deab11e95e098933cd2e99d628f19c80b89866
Modestas Vaitkevičius <info@modestasv.lt>
2025-04-10 15:47:55 +0300

    Task CRUD Toast notification (lesson 5)

--- resources/js/pages/Tasks/Index.vue
+++ resources/js/pages/Tasks/Index.vue
@@ -1,14 +1,23 @@
 <script setup lang="ts">
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Table, TableCell, TableHead, TableHeader, TableRow } from '@/components/ui/table';
+import { Button } from '@/components/ui/button';
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type Task } from '@/types';
-import { Head } from '@inertiajs/vue3';
+import { Head, router } from '@inertiajs/vue3';
+import { toast } from 'vue-sonner';
 
 interface Props {
     tasks: Task[];
 }
 
 defineProps<Props>();
+
+const deleteTask = (id: number) => {
+    if (confirm('Are you sure you want to delete this task?')) {
+        router.delete(route('tasks.destroy', { id }));
+        toast.success('Task deleted successfully');
+    }
+};
 </script>
 
 <template>
@@ -22,15 +31,15 @@ defineProps<Props>();
                     <TableHead class="w-[100px] text-right">Actions</TableHead>
                 </TableRow>
             </TableHeader>
-            <TableBody>
-                <TableRow v-for="task in tasks" :key="task.id">
-                    <TableCell>{{ task.name }}</TableCell>
-                    <TableCell :class="{ 'text-green-600': task.is_completed, 'text-red-700': !task.is_completed }">
-                        {{ task.is_completed ? 'Completed' : 'In Progress' }}
-                    </TableCell>
-                    <TableCell class="text-right">Button to edit/delete</TableCell>
-                </TableRow>
-            </TableBody>
+            <TableRow v-for="task in tasks" :key="task.id">
+                <TableCell>{{ task.name }}</TableCell>
+                <TableCell :class="{ 'text-green-600': task.is_completed, 'text-red-700': !task.is_completed }">
+                    {{ task.is_completed ? 'Completed' : 'In Progress' }}
+                </TableCell>
+                <TableCell class="text-right">
+                    <Button variant="destructive" @click="deleteTask(task.id)" class="mr-2">Delete</Button>
+                </TableCell>
+            </TableRow>
         </Table>
     </AppLayout>
 </template>
